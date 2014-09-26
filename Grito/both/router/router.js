01e3eccf6e9a20093d8aa41aa032e4e27e5d08b2
Carlos Vaca Morales <cvacam7@gmail.com>
2014-09-26 16:04:25 -0600

    Configuring Router

--- Grito/both/router/router.js
+++ Grito/both/router/router.js
@@ -1,3 +1,18 @@
 /**
  * Created by carlosvacamorales on 9/26/14.
  */
+Router.configure({
+    layoutTemplate: 'MasterLayout',
+    loadingTemplate: 'Loading',
+    notFoundTemplate: 'NotFound',
+    templateNameConverter: 'none',
+    routeControllerNameConverter: 'upperCamelCase',
+    yieldTemplates: {
+        'header': { to: 'header' },
+        'footer': { to: 'footer' }
+    }
+});
+
+Router.map(function () {
+    this.route('home', {path: '/', template:'Home'});
+});
